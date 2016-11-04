61d491618b4ec6812ed434ae08f3abc044052516
Felicia Weathers <Fel.Weathers@gmail.com>
2016-11-04 11:24:56 -0400

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MapKitLocationTracking
+//
+//  Created by Felicia Weathers on 11/4/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
